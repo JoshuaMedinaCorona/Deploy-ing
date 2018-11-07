37c78094c8999688b7ad9306399b6093bd1ed378
JoshuaMedinaCorona <JoshuaMedinaCorona@users.noreply.github.com>
2018-11-06 20:40:07 -0600

    All set
    deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.get('/api/usuarios', (req,res)=>{
+	res.send({ususarios: 'mushos'})
+});
+
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req,res) =>{
+		res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+	});
+
+}
+app.listen(process.env.PORT || 5000);
